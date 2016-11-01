3ecc274e5ccf1153da5d393931d81e579921ac69
Omar Rasool <orasool@hlkagency.com>
2016-11-01 16:17:34 -0500

    adds request product information page, assets, styles, global form styles, etc

---
+++ wp-content/themes/theme/page-request-product-information.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<div class="bluehero"></div>
+
+<div class="inner">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box">
+				<div class="mxw-1100-center">
+					<h2 class="title"><?php the_title(); ?></h2>
+					<div class="copy"><?php the_field('body_copy'); ?></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="mxw-1100-center">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6">
+				<div class="box">
+						<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+	<?php endwhile; ?>
+	<?php endif ?>
+<?php get_footer(); ?>
