a86436d7cc13a01645992bdc8145acdcb075e670
Bob Sherron <bsherron@hlkagency.com>
2016-12-01 22:32:44 -0600

    member that time you tried to use an object but it was an array? member?

--- wp-content/themes/theme/lib/functions.php
+++ wp-content/themes/theme/lib/functions.php
@@ -125,7 +125,8 @@ function cache_location_json($post_id) {
     }
     // now i got all the locs 
     $d = wp_upload_dir();
-    $write_dir =  $d->path . "../data/";
+    
+    $write_dir =  $d['path'] . "../data/";
     file_put_contents( $write_dir . "locations.json", json_encode( $all_locs ) );
   }
 		
