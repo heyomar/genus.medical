b8c982891839bdc4a77169b4b66dcf6dace2c582
Omar Rasool <orasool@hlkagency.com>
2016-12-09 15:56:19 -0600

    legal pages

--- wp-content/themes/theme/page-privacy-policy.php
+++ wp-content/themes/theme/page-privacy-policy.php
@@ -1,36 +1,30 @@
 <?php get_header(); ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<div style="background-image:url(<?php
-$image = get_field('hero_background');
-if( !empty($image) ):
-echo $image['sizes'][ 'large' ];
-endif;
-?>);" class="bluehero">
-</div>
+<div class="legal-page"><div style="background-image:url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/secondary-pages-header.jpg);" class="bluehero"></div>
 
 
 <div class="content">
 	<div class="row">
 		<div class="col-xs-12">
-			<div class="box">
+
 				<div class="mxw-1100-center">
 					<h2 class="title"><?php the_title(); ?></h2>
 				</div>
-			</div>
+
 		</div>
 	</div>
 
 	<div class="mxw-1100-center">
 		<div class="row">
-			<div class="col-xs-12 col-sm-6">
+			<div class="col-xs-12 col-sm-12">
 				<div class="box">
 						<?php the_content(); ?>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</div></div>
 
 <?php endwhile; ?>
 <?php endif ?>
