df395ae9154a51def70598aee077f86861659266
Omar Rasool <orasool@hlkagency.com>
2017-01-13 12:01:09 -0600

    fixes favions paths in the head

--- wp-content/themes/theme/header.php
+++ wp-content/themes/theme/header.php
@@ -4,12 +4,12 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name = "format-detection" content = "telephone=no">
-		
-		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-		<link rel="manifest" href="/manifest.json">
-		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff">
+
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/manifest.json">
+		<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/safari-pinned-tab.svg" color="#ffffff">
 		<meta name="theme-color" content="#ffffff">
 
 		<title><?php wp_title(); ?></title>
